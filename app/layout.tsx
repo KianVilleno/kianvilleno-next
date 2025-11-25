3bbd9c0ab5a34a665645916d8b3010f16d5247af
kianvilleno <kian@standingby.co>
2025-11-25 23:16:37 +1000

    change font and fix margins

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,8 +53,8 @@ export default function RootLayout({
         fontSans.variable
       )}
     >
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+      <body className="antialiased max-w-xl mx-4 sm:mx-auto">
+        <main className="flex-auto min-w-0 mt-6 lg:mt-20 flex flex-col px-2 md:px-0">
           {children}
           <Analytics />
           <SpeedInsights />
--- app/page.tsx
+++ app/page.tsx
@@ -11,21 +11,21 @@ export default async function Page() {
   return (
     <section>
       <h1 className="mb-8 text-xl">Kian Villeno</h1>
-      <p className="mb-4">
-        {`I'm a Filipino full stack developer living in Australia. I work at `}
+      <p className="mb-4 text-copy">
+        {`I'm a Filipino 🇵🇭 full stack developer living in Australia. I work at `}
         <Link
           href="https://standingby.co"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Standing by Co
+          Standing by
         </Link>
         {` and I've been coding for ${yearsCoding} years.`}
       </p>
-      <p className="mb-4">{`I’m learnin’ and building AI projects and when I’m off the clock I’m locked into DJing and producing. I keep tech simple make people’s lives easier and try to leave something they’ll actually remember.`}</p>
+      <p className="mb-4 text-copy">{`I’m learnin’ and building AI projects and when I’m off the clock I’m locked into DJing and producing. I keep tech simple make people’s lives easier and try to leave something they’ll actually remember.`}</p>
       {lastTrack && (
         <>
-          <p className="mb-4">
+          <p className="mb-4 text-copy">
             {"Last thing I had spinning on my Spotify rn "}
             {lastTrack?.url ? (
               <>
@@ -48,8 +48,8 @@ export default async function Page() {
           </p>
         </>
       )}
-      <h3 className="mb-4">Some of my work projects:</h3>
-      <ul className="list-disc pl-6 space-y-1">
+      <h3 className="mb-4 text-copy">Some of my work projects:</h3>
+      <ul className="list-disc pl-6 space-y-1 text-copy">
         {WORK_PROJECTS.map((project) => (
           <li key={project.name}>
             <Link href={project.url} target="_blank" rel="noopener noreferrer">
